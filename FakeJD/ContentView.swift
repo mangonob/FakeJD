72fc336d85b783dffd3cccd2f327d0173686848a
mangonob <mangonob@qq.com>
2023-03-13 20:22:31 +0800

    Initial Commit

---
+++ FakeJD/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FakeJD
+//
+//  Created by 高炼 on 2023/3/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FakeJD/FakeJDApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeJDApp.swift
+//  FakeJD
+//
+//  Created by 高炼 on 2023/3/13.
+//
+
+import SwiftUI
+
+@main
+struct FakeJDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
